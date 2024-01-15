27049b383149475fdf39c281a64423ced6ed89b1
İnanç Yücel <inancyucel@gmail.com>
2024-01-15 11:02:20 +0300

    Staff Controller Added

---
+++ Ep.Api/Controllers/StaffController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expense_Payment_System.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class StaffController : ControllerBase
+{
+    
+}
